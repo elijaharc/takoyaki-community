d564835187a665c26a17c22468ebe6644fff6354
Elijah Arcedera <eli.arcedera@gmail.com>
2021-05-18 03:17:06 +0800

    [WIP] Added region to User and removed phone number

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,10 +13,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit(:first_name, :last_name, :city, :phone_number, :email, :password, :password_confirmation)
+      u.permit(:first_name, :last_name, :region, :city, :email, :password, :password_confirmation)
     end
     devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:first_name, :last_name, :city, :phone_number, :email, :password, :current_password)
+      u.permit(:first_name, :last_name, :region, :city, :email, :password, :current_password)
     end
   end
 end
