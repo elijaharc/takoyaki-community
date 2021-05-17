d5762e884b46d791ccdbd98cd53b47515fad9eda
Elijah Arcedera <eli.arcedera@gmail.com>
2021-05-17 20:10:14 +0800

    [WIP] Implemented basic CRUD for seller pages

--- app/controllers/seller_pages_controller.rb
+++ app/controllers/seller_pages_controller.rb
@@ -1,6 +1,6 @@
 class SellerPagesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_seller_page, only: %i[ index ]
+  before_action :set_seller_page, only: %i[ index edit update destroy ]
 
   # GET /seller_page
   def index
@@ -11,42 +11,35 @@ class SellerPagesController < ApplicationController
     @seller_page = SellerPage.new
   end
 
-  # GET /seller_pages/1/edit
+  # GET /seller_pages/:id/edit
   def edit
   end
 
-  # POST /seller_pages or /seller_pages.json
+  # POST /seller_pages
   def create
     @seller_page = SellerPage.new(seller_page_params)
     @seller_page.user_id = current_user.id
 
     if @seller_page.save
-        redirect_to seller_pages_path, notice: "Seller page was successfully created."
+        redirect_to seller_page_path, notice: "Seller page was successfully created."
     else
        render action: 'new'
     end
   end
 
-  # PATCH/PUT /seller_pages/1 or /seller_pages/1.json
+  # PATCH/PUT /seller_pages/:id
   def update
-    respond_to do |format|
-      if @seller_page.update(seller_page_params)
-        format.html { redirect_to @seller_page, notice: "Seller page was successfully updated." }
-        format.json { render :show, status: :ok, location: @seller_page }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @seller_page.errors, status: :unprocessable_entity }
-      end
+    if @seller_page.update(seller_page_params)
+        redirect_to seller_pages_path, notice: "Seller page was successfully updated."
+    else
+      render action: 'edit'
     end
   end
 
-  # DELETE /seller_pages/1 or /seller_pages/1.json
+  # DELETE 
   def destroy
     @seller_page.destroy
-    respond_to do |format|
-      format.html { redirect_to seller_pages_url, notice: "Seller page was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to seller_pages_path, notice: "Seller page was successfully deleted."
   end
 
   private
