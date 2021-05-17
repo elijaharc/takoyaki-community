14b00f22382270e5fadee78806226b17a47c5bbd
Elijah Arcedera <eli.arcedera@gmail.com>
2021-05-17 19:19:36 +0800

    [WIP] Initial seller page CRUD

---
+++ app/helpers/seller_pages_helper.rb
@@ -0,0 +1,2 @@
+module SellerPagesHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :seller_pages
+  # resources :broker_stocks, only: [:create, :destroy, :show]
+
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   root to: 'home#index'
