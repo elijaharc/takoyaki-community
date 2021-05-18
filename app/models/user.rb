b63f779a50800af37289678dddd973440cb64132
Elijah Arcedera <eli.arcedera@gmail.com>
2021-05-18 09:07:20 +0800

    [WIP] Fixed error when logging in using FB when user email already exists

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,20 +10,40 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook]
 
-  has_one :seller_page
+  has_one :seller_page, dependent: :destroy
   has_many :seller_products, through: :seller_page
   validates :city, inclusion:  { in: CITIES }
 
+  # def self.from_omniauth(auth)
+  #   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+  #     user.email = auth.info.email
+  #     user.password = Devise.friendly_token[0, 20]
+  #     user.first_name = auth.info.first_name
+  #     user.last_name = auth.info.last_name 
+  #     # user.image = auth.info.image # assuming the user model has an image
+  #     # If you are using confirmable and the provider(s) you use validate emails, 
+  #     # uncomment the line below to skip the confirmation emails.
+  #     # user.skip_confirmation!
+  #   end
+  # end
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    if self.where(email: auth.info.email).exists?
+      return_user = self.where(email: auth.info.email).first
+      return_user.provider = auth.provider
+      return_user.uid = auth.uid
+      return_user.save
+    else
+      return_user = self.create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
       user.first_name = auth.info.first_name
       user.last_name = auth.info.last_name 
-      # user.image = auth.info.image # assuming the user model has an image
-      # If you are using confirmable and the provider(s) you use validate emails, 
-      # uncomment the line below to skip the confirmation emails.
-      # user.skip_confirmation!
+      end
     end
+
+    return_user
   end
 end
+
