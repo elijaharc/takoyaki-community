e4bf20067051c3af2811efc16bbdfbde0a30bb22
Elijah Arcedera <eli.arcedera@gmail.com>
2021-05-19 19:23:57 +0800

    [WIP] Added validations and availability options

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class Product < ApplicationRecord
+    belongs_to :seller_page
+    validates :product_name, :product_description, :price, presence: true
+    AVAILABILITY = [
+        ['Available', true], ['Sold out', false]
+    ].freeze
 end
