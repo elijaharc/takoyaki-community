51bead6d11b00576058e55ba93cba09d343c6297
Elijah Arcedera <eli.arcedera@gmail.com>
2021-05-19 19:24:20 +0800

    [WIP] Added dependent destroy for seller page's products

--- app/models/seller_page.rb
+++ app/models/seller_page.rb
@@ -2,6 +2,6 @@
 
 class SellerPage < ApplicationRecord
   belongs_to :user
-  has_many :seller_products, dependent: :destroy
+  has_many :products, dependent: :destroy
   validates :business_name, :business_info, presence: true
 end
