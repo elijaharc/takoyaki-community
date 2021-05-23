da342be54303d4455af5f72d12df1a8725999924
Elijah Arcedera <eli.arcedera@gmail.com>
2021-05-24 02:22:15 +0800

    [WIP] Fixed comments

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,25 +4,25 @@ class ProductsController < ApplicationController
   before_action :set_seller_page
   before_action :set_product, only: %i[ show edit update destroy ]
 
-  # GET /products
+  # GET /seller_page/:id/products
   def index
-    @products = current_user.seller_page.products
+    @products = Product.where(seller_page_id: params[:seller_page_id])
   end
 
-  # GET /products/1
+  # GET /seller_page/:id/products/1
   def show
   end
 
-  # GET /products/new
+  # GET /seller_page/:id/products/new
   def new
     @product = @seller_page.products.build
   end
 
-  # GET /products/1/edit
+  # GET /seller_page/:id/products/1/edit
   def edit
   end
 
-  # POST /products
+  # POST /seller_page/:id/products
   def create
     @product = @seller_page.products.build(product_params)
     @product.seller_page_id = current_user.seller_page.id
@@ -34,17 +34,16 @@ class ProductsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /products/1
+  # PATCH/PUT /seller_page/:id/products/1
   def update
     if @product.update(product_params)
-      byebug
       redirect_to seller_page_product_path(@product), notice: "Product was successfully updated."
     else
       render action: 'edit'
     end
   end
 
-  # DELETE /products/1
+  # DELETE /seller_page/:id/products/1
   def destroy
     @product.destroy
     redirect_back(fallback_location: root_path, notice: "Product was successfully deleted.")
