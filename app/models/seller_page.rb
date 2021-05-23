d93a0e63d9e5bd8c302e86de5c1b7c3270eb8d74
Elijah Arcedera <eli.arcedera@gmail.com>
2021-05-24 02:24:16 +0800

    [WIP] Fixed dependent destroy for when deleting seller page

--- app/models/seller_page.rb
+++ app/models/seller_page.rb
@@ -3,5 +3,6 @@
 class SellerPage < ApplicationRecord
   belongs_to :user
   has_many :products, dependent: :destroy
+  has_many :reviews, dependent: :destroy
   validates :business_name, :business_info, presence: true
 end
