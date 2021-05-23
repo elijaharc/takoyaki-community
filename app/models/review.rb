12757b55da666bc399e900d01f1332df7672ee8d
Elijah Arcedera <eli.arcedera@gmail.com>
2021-05-24 02:23:50 +0800

    [WIP] Added relationship and validations for Review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class Review < ApplicationRecord
+    belongs_to :seller_page
+    validates :comment, :rating, presence: true
+    RATING = [
+        [1], [2], [3], [4], [5]
+    ].freeze
 end
