eb882db6be2a74f2e0b95221af409b08adf03351
Elijah Arcedera <eli.arcedera@gmail.com>
2021-06-05 15:53:27 +0800

    [WIP] Passes in mock hash in User method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
-require 'spec_helper'
 
 RSpec.describe User, type: :model do
-    describe "OmniAuth" do
-        before do
-            request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
-        end
-        
-        it "sets uid" do
-            request.env["omniauth.auth"][:uid].should == '123545'
-        end
+  return_user = described_class.from_omniauth(OmniAuth.config.mock_auth[:facebook])
+
+  describe "OmniAuth" do
+    it "creates User" do
+      expect(return_user).to be_instance_of User
     end
+    
+    it "sets UID" do
+        expect(return_user.uid).to eq "12345"
+    end
+  end
 
   it { expect(described_class.new).to validate_presence_of :first_name }
   it { expect(described_class.new).to validate_presence_of :last_name }
