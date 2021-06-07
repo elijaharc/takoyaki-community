eab1837afc11e93d5af2cc6f8f9974f957c28451
Elijah Arcedera <eli.arcedera@gmail.com>
2021-06-08 00:34:18 +0800

    [WIP] Fixed error message for format of phone_number

--- app/models/seller_page.rb
+++ app/models/seller_page.rb
@@ -5,7 +5,7 @@ class SellerPage < ApplicationRecord
   has_many :products, dependent: :destroy
   has_many :reviews, dependent: :destroy
   validates :business_name, :business_info, :region, :city, presence: true
-  validates :phone_number, format: { with: /\A\d+\z/, message: "Invalid phone number format." }
+  validates :phone_number, format: { with: /\A\d+\z/, message: "cannot have letters." }
   validates :phone_number, length: {minimum: 10, maximum: 10}
 
   paginates_per 3
