76367f147a56cec5921a63eb6d55eb1673085ee1
Elijah Arcedera <eli.arcedera@gmail.com>
2021-06-08 22:24:35 +0800

    [WIP] Removed presence true for available

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@
 
 class Product < ApplicationRecord
     belongs_to :seller_page
-    validates :product_name, :product_description, :price, :available, :seller_page_id, presence: true
+    validates :product_name, :product_description, :price, :seller_page_id, presence: true
     validates :available, inclusion: { in: [true, false] }
     validates :image, file_size: { less_than_or_equal_to: 25.megabytes }
 
