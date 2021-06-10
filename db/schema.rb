9d0284857f2ca61006120ad571e57d1111feed19
AlexCitrus <kidong044@gmail.com>
2021-06-10 23:35:12 +0800

    [WIP] Seller Page Views

--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 2021_06_09_154932) do
     t.string "product_name"
     t.string "product_description"
     t.decimal "price"
-    t.boolean "available", default: true
+    t.boolean "available"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "seller_page_id"
@@ -40,13 +40,11 @@ ActiveRecord::Schema.define(version: 2021_06_09_154932) do
 
   create_table "reviews", force: :cascade do |t|
     t.string "comment"
-    t.integer "rating", default: 5
+    t.integer "rating", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "seller_page_id"
-    t.bigint "user_id"
     t.index ["seller_page_id"], name: "index_reviews_on_seller_page_id"
-    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "seller_pages", force: :cascade do |t|
@@ -89,7 +87,6 @@ ActiveRecord::Schema.define(version: 2021_06_09_154932) do
 
   add_foreign_key "products", "seller_pages"
   add_foreign_key "reviews", "seller_pages"
-  add_foreign_key "reviews", "users"
   add_foreign_key "seller_pages", "reviews"
   add_foreign_key "seller_pages", "users"
 end
