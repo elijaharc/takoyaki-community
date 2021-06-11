7701c2ae42658b83c2b1f143e039a8185a3d719e
Elijah Arcedera <eli.arcedera@gmail.com>
2021-06-11 12:36:20 +0800

    [WIP] Added validation test for :messenger_url

--- spec/models/seller_page_spec.rb
+++ spec/models/seller_page_spec.rb
@@ -8,4 +8,5 @@ RSpec.describe SellerPage, type: :model do
   it { expect(described_class.new).to validate_uniqueness_of :phone_number }
   it { expect(described_class.new).to validate_presence_of :region }
   it { expect(described_class.new).to validate_presence_of :city }
+  it { expect(described_class.new).to validate_presence_of :messenger_url }
 end
