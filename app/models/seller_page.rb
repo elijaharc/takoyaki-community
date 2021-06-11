e7a53caeae6a88ae46d403b0d83c58d1e47da5a1
Elijah Arcedera <eli.arcedera@gmail.com>
2021-06-11 12:36:32 +0800

    [WIP] Now requires messenger_url

--- app/models/seller_page.rb
+++ app/models/seller_page.rb
@@ -5,7 +5,7 @@ class SellerPage < ApplicationRecord
   belongs_to :user
   has_many :products, dependent: :destroy
   has_many :reviews, dependent: :destroy
-  validates :business_info, :region, :city, :phone_number, presence: true
+  validates :business_info, :region, :city, :phone_number, :messenger_url, presence: true
   validates :business_name, presence: true, uniqueness: true
   validates :phone_number, format: { with: /\A\d+\z/, message: "cannot have letters." }
   validates :phone_number, length: {minimum: 10, maximum: 10}
